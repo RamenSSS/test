418bcccce5e48debcb473d814186f27f69031b85
RamenSSS <144524342+RamenSSS@users.noreply.github.com>
2023-09-10 22:54:13 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,20 @@
-function doAction() {
-      alert('변경완료')
-      document.bgColor="yellow";
+function change(before, after) {
+    var queue = document.querySelectorAll(before);
+    for (var i=0; i<queue.length; i++){
+        queue[i].classList.remove(before);
+        queue[i].classList.add(after);
     }
+}
+
+function light() {
+      change(dark,light);
+      change(dark_link,light_link);
+      change(dark_table,light_table);
+}
+
+function dark() {
+      change(light,dark);
+      change(light_link,dark_link);
+      change(light_table,dark_table);
+}
+      
