01163f189e5aafc82a39a9540d44a39597a85c14
RamenSSS <144524342+RamenSSS@users.noreply.github.com>
2023-09-10 23:31:03 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 function change(before, after) {
-    var queue = document.querySelectorAll(before);
+    var queue = document.querySelectorAll('.'+before);
     for (var i=0; i<queue.length; i++){
         queue[i].classList.remove(before);
         queue[i].classList.add(after);
@@ -7,15 +7,15 @@ function change(before, after) {
 }
 
 function light() {
-    change(dark,light);
-    change(dark_link,light_link);
-    change(dark_table,light_table);
+    change('dark','light');
+    change('dark_link','light_link');
+    change('dark_table','light_table');
     document.getElementById('btn_darklight').innerText = '다크 모드'
 }
 
 function dark() {
-    change(light,dark);
-    change(light_link,dark_link);
-    change(light_table,dark_table);
+    change('light','dark');
+    change('light_link','dark_link');
+    change('light_table','dark_table');
     document.getElementById('btn_darklight').innerText = '라이트 모드'
 }
