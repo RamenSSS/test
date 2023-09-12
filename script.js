a017b5418f293660342d8c40b4c793a85acd9e7a
RamenSSS <144524342+RamenSSS@users.noreply.github.com>
2023-09-12 14:53:21 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 function change(before, after) {
-    var queue = document.querySelectorAll('.'+before);
-    for (var i=0; i<queue.length; i++){
-        queue[i].classList.remove(before);
-        queue[i].classList.add(after);
-    }
+    var queue = document.querySelector('body')
+    queue.classList.remove(before);
+    queue.classList.add(after);
 }
 
 function light() {
     change('dark','light');
-    change('dark_link','light_link');
-    change('dark_table','light_table');
     document.getElementById('btn_darklight').innerText = '다크 모드'
     status='light'
 }
 
 function dark() {
     change('light','dark');
-    change('light_link','dark_link');
-    change('light_table','dark_table');
     document.getElementById('btn_darklight').innerText = '라이트 모드'
     status='dark'
 }
@@ -27,10 +21,34 @@ function darklight() {
     else {light()}
 }
 
+function simplify() {
+    document.querySelector('body').classList.add('simple')
+    document.getElementById('btn_simple').innerText = '자세하게 보기'
+    status2='simple'
+}
+
+function unsimplify() {
+    document.querySelector('body').classList.remove('simple')
+    document.getElementById('btn_simple').innerText = '간단하게 보기'
+    status2=''
+}
+
+function simple() {
+    if (status2=='simple'){unsimplify()}
+    else {simplify()}
+}
+
 status = 'dark'
 data = location.href.split('?')
-if (data.length==2){
+if (data.length>1){
     if (data[1]=='light'){
         darklight()
+    }
+}
+
+status2 = ''
+if (data.length>2){
+    if (data[2]=='simple'){
+        simple()
     }
 }
