d0e4653c8aec549082460a5b8c23adbac424e6f6
RamenSSS <144524342+RamenSSS@users.noreply.github.com>
2023-09-29 02:09:41 +0900

    Create schscript.js

---
+++ schscript.js
@@ -0,0 +1,40 @@
+$.ajax({
+    url: '2201.csv',
+    dataType: 'text'
+}).done(setCalender);
+
+function setCalender(data) {
+  var allDates = data.split(/\r?\n|\r/);
+  var table = '<table><thead><tr><th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th></tr></thead><tbody>';
+  for (var singleDate = 0; singleDate < allDates.length; singleDate++) {
+    if (singleDate % 7 === 0) {
+      table += '<tr>'
+    }
+    table += '<td>'
+    if (!(allDates[singleDate] == '0')) {
+      var schS = allDates[singleDate].split(',');
+      for (var sch = 0; sch < schS.length; sch++) {
+        if (sch === 0) {
+          table += '<date>' + schS[0] + '</date>'
+        } else {
+          rawSch = schS[sch].split('+')
+          table += '<button class="sch">[' + rawSch[0] + ']<span class="detail_shown"> ' + rawSch[1] + '</span></button><hv><p class="detail_hidden">' + rawSch[1] + '</p>'
+          if (!(rawSch[2] == '0')) {
+            table += rawSch[2]
+          }
+          if (!(rawSch[3] == '0')) {
+            table += '<p class="mem">' + rawSch[3] + '</p>'
+          }
+          table += '</hv>'
+        }
+      }
+    }
+    table += '</td>'
+    if (singleDate % 7 === 6) {
+      table += '</tr>'
+    }
+  }
+  table += '</tbody>';
+  table += '</table>';
+  $('body').append(table);
+}
